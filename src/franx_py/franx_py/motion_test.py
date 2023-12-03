348ae3d573ec09ca0c22cc3314574a4069547081
Morris <morrisx28@gmail.com>
2023-12-03 11:50:35 +0800

    add poll coffee related function

--- src/franx_py/franx_py/motion_test.py
+++ src/franx_py/franx_py/motion_test.py
@@ -1,5 +1,3 @@
-from calendar import c
-from re import T
 import rclpy
 import rclpy.node
 import math
@@ -11,6 +9,7 @@ import cv2
 import apriltag
 import numpy as np
 import threading
+import pickle
 
 from frankx import *
 
@@ -23,6 +22,13 @@ class Pose:
     x_axis_rotation: float  # [m]
     y_axis_rotation: float  # [m]
     z_axis_rotation: float  # [m]
+
+class CoffeeParameter:
+    def __init__(self):
+        self.ready_pos = []
+        self.grasp_pos = []
+        self.poll_pos = []
+        self.put_pos = []
 
 class AprilTagDetector():
     def __init__(self):
@@ -80,6 +86,7 @@ class AprilTagDetector():
         return tag_motion
 
     def checkDetectEdge(self, x, y):
+        """Set camera detect edge (FOV)"""
         if x < 0.72 and x > -0.08 and y < 0.32 and y > -0.13:
             return True
         else:
@@ -103,6 +110,9 @@ class MotionPlaner(rclpy.node.Node):
         self.record_pose = []
         self.record_grasp_obj_pose = None
 
+        ## For record motion setting
+        self.is_recording = False
+
         ## Set Robot arm parameter ##
         self.robot = Robot(self.args.host)
         self.robot.set_default_behavior()
@@ -112,11 +122,11 @@ class MotionPlaner(rclpy.node.Node):
         ## Set Gripper parameter ##
         self.gripper = self.robot.get_gripper()
         self.gripper.gripper_speed = 0.05
-        self.gripper.gripper_force = 40
+        self.gripper.gripper_force = 60
         self.GRIPPER_MAX_WIDTH = 0.08 # m
 
         ## Test for April tag following ##
-        self.apriltag_detector = AprilTagDetector()
+        # self.apriltag_detector = AprilTagDetector()
         self.base_pos = None
         self.stop_following = False
 
@@ -125,21 +135,56 @@ class MotionPlaner(rclpy.node.Node):
         """Cartesian space coordinate"""
         return Affine(0.3 + x, y, 0.5 + z, roll, pitch, yaw)
 
-    def recordPoseTrajectory(self):
+    def recordPose(self):
         self.record_pose.append(self.robot.current_pose())
         print("Record pose: {}, {}, {}".format(self.robot.current_pose().x, self.robot.current_pose().y, self.robot.current_pose().z))
 
-    def replayRecordTrajectory(self):
-        """only support point to point trajectory"""
-        if len(self.record_pose) != 0:
-            for pose in self.record_pose:
-                success = self.moveToTarget(pose, "Linear")
-                if not success:
-                    print("Fail to move target point: {}, {}, {}".format(pose.x, pose.y, pose.z))
-                    break
-            self.record_pose = [] # reset record list
+    def startRecordTraj(self):
+        self.is_recording = True
+        self.record_thread = threading.Thread(target=self.recordPoseTrajectory)
+        self.record_thread.start()
+
+    def stopRecordTraj(self):
+        self.is_recording = False
+        if self.record_thread is not None:
+            self.record_thread.join()
+            print("stop recording")
+        else:
+            print("record not start yet")
+
+    def recordPoseTrajectory(self, collect_freq = 10):
+        """Default collect frequency is 10hz"""
+        while self.is_recording:
+            self.record_pose.append(self.robot.current_pose())
+            print("Record pose: {}, {}, {}".format(self.robot.current_pose().x, self.robot.current_pose().y, self.robot.current_pose().z))
+            time.sleep(1 / collect_freq) 
+
+    def replayRecordTrajectory(self, record_pos_list, motion_type = "Linear", cycle = 1):
+        """support follow waypoints and impedence trajectory"""
+        if len(record_pos_list) != 0:
+            if motion_type == "Linear":
+                for _ in range(cycle):
+                    for pose in record_pos_list:
+                        if not self.moveToTarget(pose, "Linear"):
+                            print("Fail to move target point: {}, {}, {}".format(pose.x, pose.y, pose.z))
+                            break
+            elif motion_type == "Impedence":
+                impedance_motion = ImpedanceMotion(400.0, 30.0) # translational stiffness, rotational stiffness
+                execute_thread = self.robot.move_async(impedance_motion)
+                time.sleep(0.1)
+                for _ in range(cycle):
+                    for pose in record_pos_list:
+                        print('target: ', impedance_motion.target)
+                        impedance_motion.target = pose
+                        time.sleep(0.5) # operation frequency 2hz
+                impedance_motion.finish()
+                execute_thread.join()
+            # self.record_pose = [] # reset record list
         else:
             print("Record pose list is empty")
+    
+    def clearRecordTrajectory(self):
+        self.record_pose = []
 
     def moveToTarget(self, pose: Affine, motion_type = "WayMotion"):
         """Target point coordinate should be based on end effector"""
@@ -171,6 +216,8 @@ class MotionPlaner(rclpy.node.Node):
         is_grasp = self.gripper.clamp()
         if is_grasp:
             print("object grasp sucess")
+            return True
+        return False
         
     def getRobotState(self):
         self.robot_state = self.robot.read_once()
@@ -181,8 +228,96 @@ class MotionPlaner(rclpy.node.Node):
         # return state
     
     def setGraspObjPos(self):
-        self.record_grasp_obj_pose = self.robot.current_pose()
-        print("obj pose: {}, {}, {}".format(self.robot.current_pose().x, self.robot.current_pose().y, self.robot.current_pose().z))
+        if len(self.record_pose) != 0:
+            self.grasp_pos = self.record_pose
+            self.clearRecordTrajectory()
+            print("Set grasp pose sucess")
+        else:
+            print("Set grasp pose fail")
+    #     self.record_grasp_obj_pose = self.robot.current_pose()
+    #     print("obj pose: {}, {}, {}".format(self.robot.current_pose().x, self.robot.current_pose().y, self.robot.current_pose().z))
+
+    def setReadyPos(self):
+        if len(self.record_pose) != 0:
+            self.ready_pos = self.record_pose
+            self.clearRecordTrajectory()
+            print("Set ready pose sucess")
+        else:
+            print("Set ready pose fail")
+
+    def setPollPos(self):
+        if len(self.record_pose) != 0:
+            self.poll_pos = self.record_pose
+            self.clearRecordTrajectory()
+            print("Set poll pose sucess")
+        else:
+            print("Set poll pose fail")
+
+    def setPutCup(self):
+        if len(self.record_pose) != 0:
+            self.put_pos = self.record_pose
+            self.clearRecordTrajectory()
+            print("Set put cup sucess")
+        else:
+            print("Set put cup pose fail")
+
+    def getCup(self):
+        if self.grasp_pos is not None:
+            self.replayRecordTrajectory(self.grasp_pos)
+            return self.closeGripper()
+        print("Grasp trajectory not set yet")
+        return False
+    
+    def putBackCup(self):
+        if self.put_pos is not None:
+            self.replayRecordTrajectory(self.put_pos)
+
+    def pollWater(self, cycle):
+        if self.poll_pos is not None:
+            self.replayRecordTrajectory(self.poll_pos, motion_type='Impedence', cycle=cycle)
+
+    def readyPos(self):
+        if self.ready_pos is not None:
+            self.replayRecordTrajectory(self.ready_pos)
+    
+    def saveParameter(self):
+        param = CoffeeParameter()
+        try:
+            param.ready_pos = self.AffineToPose(self.ready_pos)
+            param.grasp_pos = self.AffineToPose(self.grasp_pos)
+            param.poll_pos = self.AffineToPose(self.poll_pos)
+            param.put_pos = self.AffineToPose(self.put_pos)
+            file_name = "coffee_param"
+            with open(file_name, 'wb+') as f:
+                pickle.dump(param, f)
+        except Exception as e:
+            print("Fail to save parameter")
+    
+    def loadParameter(self):
+        try:
+            file_name = "coffee_param"
+            with open(file_name, 'rb') as f:
+                coffee_param = pickle.load(f)
+            self.ready_pos = self.PoseToAffine(coffee_param.ready_pos)
+            self.grasp_pos = self.PoseToAffine(coffee_param.grasp_pos)
+            self.poll_pos = self.PoseToAffine(coffee_param.poll_pos)
+            self.put_pos = self.PoseToAffine(coffee_param.put_pos)
+        except Exception as e:
+            print("Fail to load parameter")
+    
+    def AffineToPose(self, affine_list):
+        pose_list = []
+        for affine in affine_list:
+            pose = [affine.x, affine.y, affine.z, affine.a, affine.b, affine.c]
+            pose_list.append(pose)
+        return pose_list
+    
+    def PoseToAffine(self, pose_list):
+        affine_list = []
+        for pose in pose_list:
+            affine = Affine(pose[0], pose[1], pose[2], pose[3], pose[4], pose[5])
+            affine_list.append(affine)
+        return affine_list
 
     def graspObject(self):
         if self.record_grasp_obj_pose is not None:
@@ -197,54 +332,55 @@ class MotionPlaner(rclpy.node.Node):
                 self.closeGripper()
                 return True
             return False
+        
+    def pollCoffee(self):
+        if self.getCup():
+            self.readyPos()
+            ## Stage 1 ##
+            self.pollWater(6)
+            self.readyPos()
+            time.sleep(15)
+            ## Stage 2 ##
+            self.pollWater(10)
+            self.readyPos()
+            time.sleep(6)
+            ## Stage 3
+            self.pollWater(12)
+            self.readyPos()
+            self.putBackCup()
+            self.openGripper()
+            self.moveToHome()
+        else:
+            print("Cup is not exist")
     
+    def startPollCoffee(self):
+        pass
     
     def test(self):
-        motion = WaypointMotion([
-            Waypoint(Affine(0.55676, 0.01219, 0.529)),
-            Waypoint(Affine(0.54094, 0.01607, 0.529)),
-            Waypoint(Affine(0.52512, 0.01996, 0.529)),
-            Waypoint(Affine(0.50929, 0.02384, 0.529)),
-            Waypoint(Affine(0.49347, 0.02773, 0.529)),
-            Waypoint(Affine(0.47765, 0.03161, 0.529)),
-            Waypoint(Affine(0.47078, 0.02072, 0.529)),
-            Waypoint(Affine(0.4669, 0.00489, 0.529)),
-            Waypoint(Affine(0.46301, -0.01093, 0.529)),
-            Waypoint(Affine(0.45913, -0.02675, 0.529)),
-            Waypoint(Affine(0.45524, -0.04, 0.529)),
-            Waypoint(Affine(0.45136, -0.05839, 0.529)),
-            Waypoint(Affine(0.45733, -0.06825, 0.529)),
-            Waypoint(Affine(0.47315, -0.07213, 0.529)),
-            Waypoint(Affine(0.48897, -0.07602, 0.529)),
-            Waypoint(Affine(0.50479, -0.0799, 0.529)),
-            Waypoint(Affine(0.52062, -0.08378, 0.529)),
-            Waypoint(Affine(0.53644, -0.08767, 0.529)),
-            Waypoint(Affine(0.54928, -0.08663, 0.529)),
-            Waypoint(Affine(0.55316, -0.0708, 0.529)),
-            Waypoint(Affine(0.55704, -0.05498, 0.529)),
-            Waypoint(Affine(0.56093, -0.03916, 0.529)),
-            Waypoint(Affine(0.56481, -0.02334, 0.529)),
-            Waypoint(Affine(0.5687, -0.00752, 0.529)),
-        ])
-
-        self.robot.move(motion)
+        self.pollWater(1)
+        # if self.getCup():
+        #     self.readyPos()
+            # Stage 1 ##
+            # self.pollWater(3)
+            # self.readyPos()
+            # time.sleep(15)
+            # ## Stage 2 ##
+            # self.pollWater(5)
+            # self.readyPos()
+            # time.sleep(6)
+            # ## Stage 3
+            # self.pollWater(6)
+            # self.readyPos()
+            # self.putBackCup()
+            # self.openGripper()
+            # self.moveToHome()
+        
 
         # if (self.graspObject()):
         #     self.replayRecordTrajectory()
         #     self.openGripper()
         # self.moveToHome()
 
-        # tag_pos = self.apriltag_detector.getTagPosition()
-        # if tag_pos is not None and self.base_pos is not None:
-        #     motion = self.apriltag_detector.getTagMotion()
-        #     print(motion)
-        #     target_pos = Affine(self.base_pos.x + motion[0], 0.0,self.base_pos.z + motion[1])
-        #     self.moveToTarget(target_pos)
-
-        # target_pos = Affine(0.35, 0.0 , 0.05)
-        # self.moveToTarget(target_pos)
-
-        # print("Test finish")
     def goToBaseFrame(self):
         tag_pos = self.apriltag_detector.getTagPosition()
         if tag_pos is not None:
@@ -255,6 +391,9 @@ class MotionPlaner(rclpy.node.Node):
             self.base_pos = self.robot.current_pose()
 
     def followAprilTag(self):
+        self.impedance_motion = ImpedanceMotion(300.0, 20.0)
+        self.execute_thread = self.robot.move_async(self.impedance_motion)
+        time.sleep(0.1)
         while True:
             if self.stop_following:
                 break
@@ -262,8 +401,9 @@ class MotionPlaner(rclpy.node.Node):
             if tag_pos is not None and self.base_pos is not None:
                 motion = self.apriltag_detector.getTagMotion()
                 # print("Excute motion: {}".format(motion))
-                target_pos = Affine(self.base_pos.x + motion[0], 0.0,self.base_pos.z + motion[1])
-                self.moveToTarget(target_pos)
+                # target_pos = Affine(self.base_pos.x + motion[0], 0.0,self.base_pos.z + motion[1])
+                self.impedance_motion.target = Affine(self.base_pos.x + motion[0], 0.0,self.base_pos.z + motion[1])
+                # self.moveToTarget(target_pos)
                 time.sleep(0.1)
 
     def startFollow(self):
@@ -273,7 +413,10 @@ class MotionPlaner(rclpy.node.Node):
 
     def stopFollowing(self):
         self.stop_following = True
+        self.impedance_motion.finish()
+        self.execute_thread.join()
         self.follow_tag_thread.join()
+
 
 def main():
     rclpy.init()
@@ -285,11 +428,19 @@ def main():
         "open": controller.openGripper,
         "close": controller.closeGripper,
         "grasp": controller.setGraspObjPos,
-        "replay": controller.replayRecordTrajectory,
-        "set": controller.recordPoseTrajectory,
+        "poll": controller.setPollPos,
+        "set": controller.recordPose,
+        "rectraj": controller.startRecordTraj,
+        "stoprec": controller.stopRecordTraj,
+        "ready": controller.setReadyPos,
+        "put": controller.setPutCup,
         "base": controller.goToBaseFrame,
         "stop": controller.stopFollowing,
         "follow": controller.startFollow,
+        "clear": controller.clearRecordTrajectory,
+        "save": controller.saveParameter,
+        "load": controller.loadParameter,
+        "coffee": controller.pollCoffee,
     }
     while True:
         try:
@@ -297,7 +448,7 @@ def main():
             if cmd in command_dict:
                 command_dict[cmd]()
             elif cmd == "exit":
-                controller.apriltag_detector.stopDetection()
+                # controller.apriltag_detector.stopDetection()
                 break
         except Exception as e:
             traceback.print_exc()
